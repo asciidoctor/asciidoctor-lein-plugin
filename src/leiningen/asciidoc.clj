237318e295cdc22cb43dea48b910ee920f7f10f2
Vladislav Bauer <bauer.vlad@gmail.com>
2014-08-18 17:32:57 +0700

    Add help for configuration parameters

--- src/leiningen/asciidoc.clj
+++ src/leiningen/asciidoc.clj
@@ -156,6 +156,22 @@
 (defn asciidoc
   "Generate documentation using Asciidoctor.
 
+  Configure :asciidoc configuration parameter in the file project.clj using following options:
+    :sources           - List of glob patterns to define input sources.
+    :excludes          - List of glob patterns to prevent processing of some asciidoc files.
+    :to-dir            - Target directory.
+    :compact           - Remove blank lines.
+    :header-footer     - Suppress or allow the document header and footer in the output.
+    :footer            - Suppress or allow the document footer in the output.
+    :toc               - Add table of contents. Possible values: :auto, :left, :right.
+    :toc-title         - Change title of the TOC.
+    :toc-levels        - Set a deep of ToC levels. Possible values: 1, 2 (default), 3, 4, 5.
+    :title             - Configure the title of document.
+    :format            - Backend output file format: :html, :html5, :docbook, :docbook45, :docbook5
+    :doctype           - Document type: :article, :book, :manpage, :inline.
+    :source-highlight  - Enable syntax hightlighter for source codes.
+    :extract-css       - Extract CSS resources in the output directory.
+
   Usage:
     lein asciidoc"
 
