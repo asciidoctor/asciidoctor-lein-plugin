0f1212390cef5bc7069929e55e3d47226cfc829a
Vladislav Bauer <bauer.vlad@gmail.com>
2014-08-19 02:48:57 +0700

    lein-release plugin: bumped version from 0.1.5 to 0.1.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-asciidoc "0.1.5"
+(defproject lein-asciidoc "0.1.6-SNAPSHOT"
   :description "A Leiningen plugin for generating documentation using Asciidoctor."
   :url "https://github.com/vbauer/lein-asciidoc"
   :license {:name "Eclipse Public License"
