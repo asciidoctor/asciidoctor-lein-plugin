8d03c384efd995cb29ebb14d660d082a42ea0713
Vladislav Bauer <bauer.vlad@gmail.com>
2014-08-19 02:42:17 +0700

    lein-release plugin: preparing 0.1.4 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-asciidoc "0.1.4-SNAPSHOT"
+(defproject lein-asciidoc "0.1.4"
   :description "A Leiningen plugin for generating documentation using Asciidoctor."
   :url "https://github.com/vbauer/lein-asciidoc"
   :license {:name "Eclipse Public License"
