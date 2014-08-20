386eafb55972c78917b6cd57ce38a3b2e83d5717
Vladislav Bauer <bauer.vlad@gmail.com>
2014-08-21 04:59:22 +0700

    Add an example project, update documentation

---
+++ example/project.clj
@@ -0,0 +1,13 @@
+(defproject example "0.1.0-SNAPSHOT"
+  :description "Simple example of using lein-asciidoc"
+  :url "https://github.com/vbauer/lein-asciidoc"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :plugins [[lein-asciidoc "0.1.8-SNAPSHOT"]]
+
+  :asciidoc {:sources "README.asciidoc"
+             :to-dir "generated"
+             :extract-css true
+             :source-highlight true
+             :toc :left})
