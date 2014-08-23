d7fb420677b75aa9dd7705566ff5e922937e92e4
Vladislav Bauer <bauer.vlad@gmail.com>
2014-08-24 02:50:26 +0700

    lein-release plugin: bumped version from 0.1.8 to 0.1.9-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-asciidoctor "0.1.8"
+(defproject lein-asciidoctor "0.1.9-SNAPSHOT"
   :description "A Leiningen plugin for generating documentation using Asciidoctor."
   :url "https://github.com/asciidoctor/asciidoctor-lein-plugin"
   :license {:name "The MIT License"
