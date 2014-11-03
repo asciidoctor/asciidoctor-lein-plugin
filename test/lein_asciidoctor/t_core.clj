02720c75b1f239fe74f22167ec1c4eb506d8589a
Vladislav Bauer <bauer.vlad@gmail.com>
2014-11-04 03:32:55 +0600

    Add unit test: check lein task task for documentation generation

--- test/lein_asciidoctor/t_core.clj
+++ test/lein_asciidoctor/t_core.clj
@@ -3,16 +3,38 @@
   (:use [midje.sweet]
         [midje.util :only [testable-privates]]
         [clojure.string :only [blank?]])
-  (:require [lein-asciidoctor.core]))
+  (:require [lein-asciidoctor.core]
+            [me.raynes.fs :as fs]))
 
 
 ; Configurations
 
+(def ^:private DEF_OUTPUT "./test-out/")
+(def ^:private DEF_CONFIG
+  {:asciidoctor {:sources "example/README.adoc"
+                 :to-dir DEF_OUTPUT
+                 :extract-css true
+                 :source-highlight true
+                 :toc :left}})
+
 (testable-privates
   lein-asciidoctor.core
     load-resource
+    asciidoctor
+    RESOURCE_ASCIIDOCTOR
     RESOURCE_ASCIIDOCTOR_DEFAULT
     RESOURCE_CODERAY_ASCIIDOCTOR)
+
+
+; Helper functions
+
+(defn- run-generator []
+  (try
+    (asciidoctor DEF_CONFIG)
+    (and
+     (fs/exists? (str DEF_OUTPUT (var-get RESOURCE_ASCIIDOCTOR)))
+     (fs/exists? (str DEF_OUTPUT (var-get RESOURCE_CODERAY_ASCIIDOCTOR))))
+    (finally (fs/delete-dir DEF_OUTPUT))))
 
 
 ; Tests
@@ -21,3 +43,6 @@
   (blank? (load-resource nil)) => true
   (blank? (load-resource (var-get RESOURCE_ASCIIDOCTOR_DEFAULT))) => false
   (blank? (load-resource (var-get RESOURCE_CODERAY_ASCIIDOCTOR))) => false)
+
+(fact "Check documentation generator"
+  (run-generator) => true)
