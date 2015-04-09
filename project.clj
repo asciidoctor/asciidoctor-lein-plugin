d40819003cc98f999a13d6b3c0d5861469051faa
Vladislav Bauer <bauer.vlad@gmail.com>
2015-04-10 04:59:21 +0600

    Remove usage of old Maven repository

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :license {:name "The MIT License"
             :url "https://github.com/asciidoctor/asciidoctor-lein-plugin/blob/master/LICENSE.adoc"}
 
-  :repositories [["lordofthejars" "http://dl.bintray.com/lordofthejars/maven"]]
-
   :dependencies [[me.raynes/fs "1.4.6" :exclusions [org.clojure/clojure]]
                  [org.asciidoctor/asciidoctorj "1.5.2" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/slf4j-nop "1.7.12"]]
